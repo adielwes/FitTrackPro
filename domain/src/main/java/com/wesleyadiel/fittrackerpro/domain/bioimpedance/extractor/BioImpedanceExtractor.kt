58a7bf6e359ec2d1912ce2a958369081385e75e5
Adiel W. <adielwes@gmail.com>
2025-09-28 16:06:49 -0300

    Revert "feat: add AI-powered bioimpedance extraction with LLM image parsing"
    This reverts commit 141d78c26e291815e3df6f17da23d77011000638.

--- domain/src/main/java/com/wesleyadiel/fittrackerpro/domain/bioimpedance/extractor/BioImpedanceExtractor.kt
+++
@@ -1,8 +0,0 @@
-package com.wesleyadiel.fittrackerpro.domain.bioimpedance.extractor
-
-import android.graphics.Bitmap
-import com.wesleyadiel.fittrackerpro.domain.bioimpedance.model.BioImpedanceData
-
-interface BioImpedanceExtractor {
-    suspend fun extractFromImage(image: Bitmap): BioImpedanceData
-}
